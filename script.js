3bbe6a35015122042ba4ef5419d6d0453466aab6
B3lk00 <belmins1617@gmail.com>
2026-05-21 20:42:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Sajt je učitan.");
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("mouseenter", () => {
+        dropdown.classList.add("open");
+    });
+
+    dropdown.addEventListener("mouseleave", () => {
+        dropdown.classList.remove("open");
+    });
+});
