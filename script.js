a3755c7da3397c4506147424f4311071ecc0181e
B3lk00 <belmins1617@gmail.com>
2026-05-24 00:18:57 +0200

    Implement lightbox for image gallery
    Added lightbox functionality to display images.

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ dropdowns.forEach((dropdown) => {
         dropdown.classList.remove("open");
     });
 });
+
+/*lightbox*/
+
+const images = [
+    "images/caffe1.jpg",
+    "images/caffe2.jpg",
+    "images/caffe3.jpg",
+    "images/caffe4.jpg"
+];
+
+let currentImage = 0;
+
+function openLightbox(index) {
+    currentImage = index;
+    document.getElementById("lightbox-img").src = images[currentImage];
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function changeImage(direction) {
+    currentImage += direction;
+
+    if (currentImage < 0) {
+        currentImage = images.length - 1;
+    }
+
+    if (currentImage >= images.length) {
+        currentImage = 0;
+    }
+
+    document.getElementById("lightbox-img").src = images[currentImage];
+}
